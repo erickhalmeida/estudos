ffad697fc90eb30759cac271aa6bffe0155d3d81
Erick Almeida <97368097+erickhalmeida@users.noreply.github.com>
2023-11-07 23:45:57 -0300

    Aula: 25. Template expressions

--- React/Udemy/react_do_zero_a_maestria/02-fundamentos_do_react/projeto/src/App.js
+++ React/Udemy/react_do_zero_a_maestria/02-fundamentos_do_react/projeto/src/App.js
@@ -1,13 +1,16 @@
-// Components
-import FirstComponent from './components/FirstComponent';
 // Styles
 import './App.css';
+// Components
+import FirstComponent from './components/FirstComponent';
+import TemplateExpressions from './components/TemplateExpressions';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
       <FirstComponent />
+      <TemplateExpressions />
     </div>
   );
 }
