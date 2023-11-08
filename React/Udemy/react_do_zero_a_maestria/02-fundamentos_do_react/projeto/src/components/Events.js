8a1947ed3f35b7493cedc292c1cd808c141ddcb4
Erick Almeida <97368097+erickhalmeida@users.noreply.github.com>
2023-11-08 00:01:51 -0300

    Aula: 28. Funções nos eventos

--- React/Udemy/react_do_zero_a_maestria/02-fundamentos_do_react/projeto/src/components/Events.js
+++ React/Udemy/react_do_zero_a_maestria/02-fundamentos_do_react/projeto/src/components/Events.js
@@ -10,6 +10,14 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou!")}>Clique aqui também!</button>
+                <button onClick={() => {
+                    if (true) {
+                        console.log("Isso não deveria exisitr =)");
+                    }
+                }}>Clica aqui, por favor.</button>
+            </div>
         </div>
     );
 
