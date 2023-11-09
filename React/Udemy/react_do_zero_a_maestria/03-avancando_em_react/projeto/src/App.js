025f7e73ca526252ddf1d816d2194cc703e8d5dd
Erick Almeida <97368097+erickhalmeida@users.noreply.github.com>
2023-11-08 22:32:21 -0300

    Aula: 33. Imagens em src

--- React/Udemy/react_do_zero_a_maestria/03-avancando_em_react/projeto/src/App.js
+++ React/Udemy/react_do_zero_a_maestria/03-avancando_em_react/projeto/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 
+import Img2 from './assets/img2.jpg';
+
 function App() {
   return (
     <div className="App">
       <h1>Avançando em React</h1>
       <div>
         <img src="/img1.jpg" alt="" />
+      </div>
+      <div>
+        <img src={Img2} alt="" />
       </div>
     </div>
   );
