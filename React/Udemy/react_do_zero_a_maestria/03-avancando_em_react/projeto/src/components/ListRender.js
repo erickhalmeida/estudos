bd8b33be174d179c632f527e318721a20194a857
Erick Almeida <97368097+erickhalmeida@users.noreply.github.com>
2023-11-10 01:00:29 -0300

    Aula: 37. A propriedade key

--- React/Udemy/react_do_zero_a_maestria/03-avancando_em_react/projeto/src/components/ListRender.js
+++ React/Udemy/react_do_zero_a_maestria/03-avancando_em_react/projeto/src/components/ListRender.js
@@ -4,12 +4,22 @@ const ListRender = () => {
 
     const [list] =  useState(["Matheus", "Pedro", "Josias", "Maria"]); 
 
+    const [users] = useState([
+      { id: 1, nome: "João"},
+      { id: 2, nome: "Fernando"},
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ))}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.nome}</li>
+          ))}
         </ul>
     </div>
   );
